54ad5290c3b3ba544419714fc191015fd22c3659
Bezawork <bezaworkalx@gmail.com>
2023-04-29 23:22:27 -0700

    Write a type-annotated function floor which takes a float n as argument and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This file contains a function that has type-annotation
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """concats two strings"""
+    return math.floor(n)
