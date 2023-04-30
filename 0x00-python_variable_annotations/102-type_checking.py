62f41a0fe002308874ce09b2740ce3a5fa334ecc
Bezawork <bezaworkalx@gmail.com>
2023-04-30 01:58:16 -0700

    Use mypy to validate the given piece of code

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece
+of code and apply any necessary changes"""
+from typing import List
+
+
+def zoom_array(lst: List, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x: List = zoom_array(array)
+
+zoom_3x: List = zoom_array(array, 3)
