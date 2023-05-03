4168b087fce2c4edb88d2f9b1b178420ce7863f0
Bezawork <bezaworkalx@gmail.com>
2023-05-03 03:13:29 -0700

    Fix pycodestyle error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''This file imports async_generator from the previous task
 then write a coroutine called async_comprehension that takes no arguments'''
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     '''Return yields from async_generator'''
