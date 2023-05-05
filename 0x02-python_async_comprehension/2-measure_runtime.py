e2e4c86d02de45912500096c0602327f1671be84
Bezawork <bezaworkalx@gmail.com>
2023-05-05 16:24:59 +0000

    Run async_comprehension four times in parallel instead of 1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,7 +10,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     '''Calculate time taken'''
     start_time = time.monotonic()
-    my_list = async_comprehension()
-    await asyncio.gather(my_list)
+    my_list = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*my_list)
     end_time = time.monotonic()
     return end_time - start_time
