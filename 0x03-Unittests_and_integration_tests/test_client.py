3154373abffd6d5efe8db9135904cd7caf92f3c7
Bezawork <bezawork-pr@gmail.com>
2023-05-22 06:00:31 -0700

    unit-test GithubOrgClient._public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This file contains test classes"""
 import unittest
+import mock
 from unittest.mock import patch
 import client
 from client import GithubOrgClient
@@ -20,6 +21,14 @@ class TestGithubOrgClient(unittest.TestCase):
         my_client.org()
         string = "https://api.github.com/orgs/"
         mocked.assert_called_once_with(string + org_name)
+
+    def test_public_repos_url(self):
+        """test_public_repos_url"""
+        url = 'client.GithubOrgClient._public_repos_url'
+        with mock.patch(url, new_callable=mock.PropertyMock) as mockMyP:
+            mockMyP.return_value = {"resp": "alx"}
+            test = GithubOrgClient("alx")
+            self.assertEqual(test._public_repos_url, mockMyP.return_value)
 
 
 if __name__ == "__main__":
