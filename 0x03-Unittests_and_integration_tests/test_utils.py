4335f2e2b0f25d33946bfb8932ce8a3f30a334e9
Bezawork <bezawork-pr@gmail.com>
2023-05-22 02:21:03 -0700

    Use parameterized.expand and access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Implement the TestAccessNestedMap.test_access_nested_map
+method to test that the method returns
+what it is supposed to"""
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittes.TestCase):
+    """TestCase"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
+    def test_access_nested_map(self, test, path, answer):
+        """Test access_nested_map"""
+        self.assertEqual(access_nested_map(test, path), answer)
+
+
+if __name__ == "__main__":
+    unittest.main()
