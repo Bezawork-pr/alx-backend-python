bac45d13c9400c2b95ef73c11f639b9f22863d39
Bezawork <bezawork-pr@gmail.com>
2023-05-22 03:30:39 -0700

    use MagicMock to test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 method to test that the method returns
 what it is supposed to"""
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """Use the assertRaises context manager
         to test that a KeyError is raised"""
         self.assertRaises(KeyError)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestCase"""
+    @patch('utils.requests')
+    def test_get_json(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = 200 
+        mock_response.json.return_value = {'value': 'hello world'}
+        mock_requests.get.return_value = mock_response
+        result = get_json('https://bezawork-pr.github.io/hello_world.html')
+        self.assertEqual(result, {'value': 'hello world'})
 
 
 if __name__ == "__main__":
